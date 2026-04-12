5296c5cbf48e2079fe0f1d00fac1c5017c95f2a5
nakonechnaolena-ops <nakonechna.olena@e-u.edu.ua>
2026-04-12 23:43:31 +0300

    Update task05.rs

--- src/hackerrank/task05.rs
+++ src/hackerrank/task05.rs
@@ -1,1 +1,65 @@
 // https://www.hackerrank.com/challenges/apple-and-orange/problem
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    
+
+    public static string kangaroo(int x1, int v1, int x2, int v2)
+  
+    {
+     if (v1 == v2)
+     {
+        return x1 == x2 ? "YES" : "NO";
+     }
+       int dx = x2 - x1;
+       int dv = v1 - v2;
+       
+       if (dx * dv > 0 && dx % dv == 0)
+       {
+          return "YES";
+       }
+       
+          return "NO";
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int x1 = Convert.ToInt32(firstMultipleInput[0]);
+
+        int v1 = Convert.ToInt32(firstMultipleInput[1]);
+
+        int x2 = Convert.ToInt32(firstMultipleInput[2]);
+
+        int v2 = Convert.ToInt32(firstMultipleInput[3]);
+
+        string result = Result.kangaroo(x1, v1, x2, v2);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
